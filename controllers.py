ff81ede9b16fae10dc2ad6b4ea3b568af82661ba
youmin29 <babybana0829@naver.com>
2026-01-19 19:37:55 +0900

    ✨ Feat: 리마인드 알림 전송 추가

--- controllers.py
+++ controllers.py
@@ -3,7 +3,8 @@ from slack_bolt.adapter.fastapi import SlackRequestHandler
 from modules.slack import BoltApp
 from common.slack import SlackBotName
 from services import (open_create_reminder_shortcut, create_reminder,
-                      open_delete_reminder_shortcut, delete_reminder)
+                      open_delete_reminder_shortcut, delete_reminder,
+                      send_reminder_message)
 
 router = APIRouter(prefix="/slack", tags=["Slack"])
 
@@ -14,6 +15,7 @@ slack_app.shortcut("CREATE_REMINDER")(open_create_reminder_shortcut)
 slack_app.view("reminder_create_submit")(create_reminder)
 slack_app.shortcut("DELETE_REMINDER")(open_delete_reminder_shortcut)
 slack_app.view("reminder_delete_submit")(delete_reminder)
+slack_app.command("/test_reminder")(send_reminder_message)  # 리마인드 알림 테스트용
 
 
 @router.post("/events")
--- handlers.py
+++ handlers.py
@@ -1,8 +1,8 @@
 import json
-from datetime import date, datetime
+from datetime import date, datetime, timezone, timedelta
 from slack_sdk.errors import SlackApiError
 from models.reminder import Reminder, ReminderStatus
-from blocks.reminder import create_reminder_modal_view, remind_start_message_block, delete_reminder_modal_view
+from blocks.reminder import create_reminder_modal_view, remind_start_message_block, delete_reminder_modal_view, remind_alarm_message_block
 from common.slack_blocks import get_mrkdwn_block, get_header_block, get_divider_block, get_context_block
 
 class ReminderHandler():
@@ -134,3 +134,47 @@ class ReminderHandler():
                 ])
             ]
         )
+
+    def send_reminder_message(self, body):
+        today = datetime.now().date()
+        reminders = Reminder.objects(status__in=["PENDING", "ACTIVE"])
+
+        for reminder in reminders:
+            if reminder.start_date.date() <= today <= reminder.end_date.date():
+                alarm_user_list = list(set(reminder.selected_users) - set(reminder.completed_users))
+                # 알림 전송
+                self.client.chat_postMessage(
+                    channel=reminder.channel_id,
+                    text="리마인드 알림 도착",
+                    thread_ts=reminder.message_ts,
+                    blocks=remind_alarm_message_block(consts=reminder.consts, selected_users_slack_key=alarm_user_list)
+                )
+
+                reminder.last_triggered_at = today
+                if today >= reminder.end_date.date():
+                    reminder.status = ReminderStatus.DONE
+
+                    # 종료 예정 메시지 전송
+                    self.client.chat_postMessage(
+                        channel=reminder.channel_id,
+                        text="리마인드 종료 예정",
+                        thread_ts=reminder.message_ts,
+                        blocks=[get_context_block([
+                            {
+                                "type": "mrkdwn",
+                                "text": ":bulb: 오늘이 리마인드 마지막 날이에요"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": ":bulb: 아직 작업을 완료하지 못한 담당자분들은 작업 완료를 눌러주세요"
+                            }])
+                        ])
+
+                reminder.save()
+
+    @staticmethod
+    def _get_today_kst():
+        return (datetime.now(timezone.utc) + timedelta(hours=9))
+
+    def confirm_reminder(self, body):
+        print(body)
